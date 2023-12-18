87feba36d71b62db01966a59c737a942c7f7b705
Saketh Dhulipalla <sakethdhulipalla@Sakeths-MacBook-Pro-2.local>
2023-12-18 17:24:21 -0600

    test: finished integration testing

--- src/front/test/full.spec.ts
+++ src/front/test/full.spec.ts
@@ -37,7 +37,7 @@ test("check player outbut box", async ({page}) => {
   await page.getByLabel("Player 1 input").click();
   await page.getByLabel("Player 1 input").fill("Sue Bird");
   await page.getByLabel("Player 2 input").click();
-  await page.getByLabel("Player 2 input").fill("Candace Parker");
+  await page.getByLabel("Player 2 input").fill("Lisa Leslie");
   await page.click("[id='submit-button']");
   await expect(page.getByLabel("player-output")).toContainText("Sue Bird played with " +
       "Betty Lennox on the Seattle Storm in 2007");
@@ -53,18 +53,18 @@ test("check for right ticketing teams", async ({page}) => {
   await page.click("[id='submit-button']");
   await expect(page.getByLabel("player-output")).toContainText("Tickets for the " +
       "Seattle Storm here!");
-  await expect(page.getByLabel("player-output")).toContainText("Tickets for the " +
-      "Los Angeles Spark here!");
+  await expect(page.getByLabel("player-output")).toContainText("Tickets for the" +
+      " Los Angeles Sparks here!");
 })
 
 test("check if it detects old team name", async ({page}) => {
   await page.getByLabel("Player 1 input").click();
-  await page.getByLabel("Player 1 input").fill("Sue Bird");
+  await page.getByLabel("Player 1 input").fill("Astou Ndour-Fall");
   await page.getByLabel("Player 2 input").click();
-  await page.getByLabel("Player 2 input").fill("Candace Parker");
+  await page.getByLabel("Player 2 input").fill("Aiysha Smith");
   await page.click("[id='submit-button']");
   await expect(page.getByLabel("player-output")).toContainText("The San Antonio " +
-      "Silver Stars no longer exists!");
+      "Stars no longer exists!");
 })
 
 test("correct longer connection", async ({page}) => {
@@ -94,5 +94,66 @@ test("check if search history stays", async ({page}) => {
       "between Sue Bird and Candace Parker is 2");
 })
 
+test("check 1 level connection", async ({page}) => {
+  await page.getByLabel("Player 1 input").click();
+  await page.getByLabel("Player 1 input").fill("Sue Bird");
+  await page.getByLabel("Player 2 input").click();
+  await page.getByLabel("Player 2 input").fill("Breanna Stewart");
+  await page.click("[id='submit-button']");
+  await page.getByLabel("Player 1 input").click();
+  await page.getByLabel("Player 1 input").fill("Susie Hopson-Shelton");
+  await page.getByLabel("Player 2 input").click();
+  await page.getByLabel("Player 2 input").fill("Jade Melbourne");
+  await page.click("[id='submit-button']");
+  await expect(page.getByLabel("History")).toContainText("The number of " +
+      "connections between Sue Bird and Breanna Stewart is 1");
+  await expect(page.getByLabel("player-output")).toContainText("Sue Bird played with " +
+      "Breanna Stewart on the Seattle Storm in 2022");
+})
 
+test("check no input", async ({page}) => {
+  await page.getByLabel("Player 1 input").click();
+  await page.getByLabel("Player 1 input").fill("");
+  await page.getByLabel("Player 2 input").click();
+  await page.getByLabel("Player 2 input").fill("");
+  await page.click("[id='submit-button']");
+  await expect(page.getByLabel("History")).toContainText("Only one player " +
+      "submitted.");
+})
 
+test("check one input", async ({page}) => {
+  await page.getByLabel("Player 1 input").click();
+  await page.getByLabel("Player 1 input").fill("Sue Bird");
+  await page.getByLabel("Player 2 input").click();
+  await page.getByLabel("Player 2 input").fill("");
+  await page.click("[id='submit-button']");
+  await expect(page.getByLabel("History")).toContainText("Invalid second player.");
+})
+
+test("check one input for second box", async ({page}) => {
+  await page.getByLabel("Player 1 input").click();
+  await page.getByLabel("Player 1 input").fill("");
+  await page.getByLabel("Player 2 input").click();
+  await page.getByLabel("Player 2 input").fill("Sue Bird");
+  await page.click("[id='submit-button']");
+  await expect(page.getByLabel("History")).toContainText("Invalid first player.");
+})
+
+test("check invalid input or typo", async ({page}) => {
+  await page.getByLabel("Player 1 input").click();
+  await page.getByLabel("Player 1 input").fill("Sue Baird");
+  await page.getByLabel("Player 2 input").click();
+  await page.getByLabel("Player 2 input").fill("Brittneyyy Grinerrrrrr");
+  await page.click("[id='submit-button']");
+  await expect(page.getByLabel("History")).toContainText("");
+})
+
+test("check same player twice", async ({page}) => {
+  await page.getByLabel("Player 1 input").click();
+  await page.getByLabel("Player 1 input").fill("Sue Bird");
+  await page.getByLabel("Player 2 input").click();
+  await page.getByLabel("Player 2 input").fill("Sue Bird");
+  await page.click("[id='submit-button']");
+  await expect(page.getByLabel("History")).toContainText("Only one player " +
+      "submitted.");
+})
